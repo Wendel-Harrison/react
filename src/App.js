3049d72a6d53b1b3a3e87e2e051246cb3c7ebd70
Wendel Harrison <wh68639@gmail.com>
2022-08-10 22:31:30 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Meus livros</h1>
       </header>
-    </div>
-  );
+
+      <main>
+        <input type="text" placeholder='PROCURE AQUI...'>
+        </input>
+
+      
+          <ul>
+            <p>Livro 1</p>
+            <p>Livro 2</p>
+            <p>Livro 3</p>
+            <p>Livro 4</p>
+          </ul>
+        
+
+      </main>
+
+      <footer>
+         <p>Feito by Wendel Harrison</p>
+      </footer>
+    </>
+  
+  )
 }
+
 
 export default App;
